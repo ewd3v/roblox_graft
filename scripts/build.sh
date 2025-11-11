dac560e4faad9676455169b569b8e003983e0d92
ewd3v <74792428+ewd3v@users.noreply.github.com>
2025-11-11 19:45:51 +0100

    change build step to use darklua_build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-pesde x ewdev/multitarget -- build --yes \
-    --build-files src --output dist \
-    --global RUNTIME --require-mode luau \
-    --roblox # Runtimes to build for
+darklua_build --config .darklua.build.toml
