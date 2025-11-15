ffec39781af397169d85a4285c95e94319bbb14b
ewd3v <74792428+ewd3v@users.noreply.github.com>
2025-11-15 02:24:24 +0100

    create plugin

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,3 +2,6 @@
 set -euo pipefail
 
 darklua_build --config .darklua.build.toml
+
+# TODO: Move this into repo without tooling dying
+curl -L https://github.com/RadiatedExodus/LuauCeption/releases/download/0.654/Luau.LuauCeption.Compiler.0.654.luau > out/plugin/luau.luau
