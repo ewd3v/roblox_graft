d78bd2009c4c1cc66ac1c97df45715ee7dcfa2b4
ewd3v <74792428+ewd3v@users.noreply.github.com>
2025-11-15 12:50:31 +0100

    set private to false in build output

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,3 +5,7 @@ darklua_build --config .darklua.build.toml
 
 # TODO: Move this into repo without tooling dying
 curl -L https://github.com/RadiatedExodus/LuauCeption/releases/download/0.654/Luau.LuauCeption.Compiler.0.654.luau > out/plugin/luau.luau
+
+sed -i -e \
+    "s/private = true/private = false/" \
+    out/pesde.toml
